a82236b57ba9c6f7ecc846d8aa8a7836f1d47233
Saransh <saranshsingh454@gmail.com>
2024-09-29 13:14:41 -0400

    first push

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function nav() {
+    return (
+      <>
+        <div>
+          <nav>
+            <ul>
+              <li>About</li>
+              <li>Search</li>
+              <li>Contact</li> {/* Changed one of the duplicate Search items to Contact for clarity */}
+            </ul>
+          </nav>
+        </div>
+      </>
+    )
+  }
+
+export default nav
